3a638dea66fb3305b2c37722618f092632e374f2
Ronald Schlenker <ronald.schlenker@technewlogic.de>
2018-12-30 11:35:54 +0100

    moved stuff

--- src/Samples/Demo.fsx
+++ src/Samples/Demo.fsx
@@ -1,13 +1,14 @@
 ﻿
-#r @"..\packages\fsharp.data\lib\net45\FSharp.Data.dll"
-#r @"..\packages\NUnit\lib\netstandard2.0\nunit.framework.dll"
-#r @"..\packages\fsunit\lib\netstandard2.0\FsUnit.NUnit.dll"
+#r @"../../packages/fsharp.data/lib/net45/FSharp.Data.dll"
+#r @"../../packages/NUnit/lib/netstandard2.0/nunit.framework.dll"
+#r @"../../packages/fsunit/lib/netstandard2.0/FsUnit.NUnit.dll"
 #load @"../FsHttp/bin/Debug/netstandard2.0/FsHttp.fsx"
 
 open FsHttp
 open FsUnit
 open FSharp.Data
 open FSharp.Data.JsonExtensions
+
 
 http {
     GET @"https://reqres.in/api/users?page=2&delay=3"
