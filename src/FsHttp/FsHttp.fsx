18f5ec26f4de40606f8d9acffd297a033a5c9f71
Ronald Schlenker <ronald.schlenker@technewlogic.de>
2019-08-04 10:15:15 +0200

    FSX: includes

--- src/FsHttp/FsHttp.fsx
+++ src/FsHttp/FsHttp.fsx
@@ -1,6 +1,10 @@
 #nowarn "211"
-#I __SOURCE_DIRECTORY__
-#I "../../../../../packages/FSharp.Data/lib/netstandard2.0"  // dev + paket
+
+#I __SOURCE_DIRECTORY__                                      // FsHttp.dll (dev after build)
+#I "bin/debug/netstandard2.0"                                // FsHttp.dll (dev before build)
+
+#I "../../packages/FSharp.Data/lib/netstandard2.0"           // dev before build + paket
+#I "../../../../../packages/FSharp.Data/lib/netstandard2.0"  // dev after build + paket
 #I "../../../packages/FSharp.Data/lib/netstandard2.0"        // package + paket
 #I "../packages/FSharp.Data.3.1.1/lib/netstandard2.0"        // package + nuget
 
@@ -9,12 +13,14 @@
 #r "Fsharp.Data.dll"
 #r "FsHttp.dll"
 
+open FsHttp
+
 // for debugging purpose
 // #r "./bin/Debug/netstandard2.0/FsHttp.dll"
 
-type PrintableResponse = | PrintableResponse of FsHttp.Domain.Response
+type PrintableResponse = | PrintableResponse of Response
 
-let printTransformer (r:FsHttp.Domain.Response) =
+let printTransformer (r:Response) =
     match r.printHint.isEnabled with
     | true -> (PrintableResponse r) :> obj
     | false -> null
@@ -22,6 +28,6 @@ fsi.AddPrintTransformer printTransformer
 
 let printer (r:PrintableResponse) =
     let (PrintableResponse inner) = r
-    try FsHttp.FsiPrinting.print inner
+    try FsiPrinting.print inner
     with ex -> ex.ToString()
 fsi.AddPrinter printer
