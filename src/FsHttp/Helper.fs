edab88e5448e83e4acebe6a649cbdca5454398ef
Mefgalm <mefgalm@gmail.com>
2020-08-19 15:42:17 +0300

    add helpers

--- src/FsHttp/Helper.fs
+++ src/FsHttp/Helper.fs
@@ -17,6 +17,17 @@ let fromBase64 (s: string) =
 
 let substring (s:string) maxLength = string(s.Substring(0, Math.Min(maxLength, s.Length)))
 
+let encodeUrlParam param =
+    (Uri.EscapeUriString param).Replace("&", "%26").Replace("#", "%23")
+            
+let appendQueryToUrl queryParams (url: string) =
+    match queryParams with
+    | [] -> url
+    | query ->
+        url
+        + if url.Contains "?" then "&" else "?"
+        + String.concat "&" (query |> List.map (fun (k, v) -> encodeUrlParam k + "=" + encodeUrlParam v))
+
 // TODO: Test
 let urlCombine (url1:string) (url2:string) =
     (url1.TrimEnd [|'/'|]) + "/" + (url2.TrimStart [|'/'|])
