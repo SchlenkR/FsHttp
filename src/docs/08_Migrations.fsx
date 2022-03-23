5e746ab9b6d8a266c5da86eb0d1fd84d30af9d36
SchlenkR <ronald.schlenker@binarygears.de>
2022-03-23 15:38:56 +0100

    Docu

---
+++ src/docs/08_Migrations.fsx
@@ -0,0 +1,35 @@
+(**
+---
+title: Migrations
+category: Documentation
+categoryindex: 1
+index: 8
+---
+*)
+
+(**
+## Migrations
+
+### To v9 (Breaking Changes)
+
+* `http...` Builders: There is now only a single `http` builder, that is equivalent to the former `httpLazy` builder. To achieve the behaviour of the removed builders, please use:
+* `httpLazy` -> `http { ... }`
+* `http` -> `http { ... } |> Request.send`
+* `httpAsync` -> `http { ... } |> Request.sendAsync`
+* `httpLazyAsync` -> `http { ... } |> Request.toAsync`
+* `httpMessage` -> `http { ... } |> Request.toMessage`
+* see also: [./src/Tests/BuildersAndSignatures.fs](Tests in BuildersAndSignatures.fs)
+* Renamed type `LazyHttpBuilder` -> `HttpBuilder`
+* Renamed `Request.buildAsync` -> `Request.toAsync`
+* Removed `send` and `sendAsync` builder methods
+* Changed request and response printing (mostly used in FSI)
+* Printing related custom operations change in names and behaviour
+* `Dsl` / `DslCE` namespaces: There is no need for distinction of both namespaces. It is now sufficient to `open FsHttp` only.
+* The `HttpBuilder<'context>` is replaced by `IBuilder<'self>`, so that the CE methods work directly on the `HeaderContext`, `BodyContext`, and `MultipartContext` directly. This simplifies things like mixing Dsl and DslCE, pre-configuring and chaining requests.
+* The global configuration is now in the `FsHttp.GlobalConfig` module. The `Config` module is only for functions on request contexts.
+* QueryParams is `(string * obj) list` now
+* Use of System.Text.Json as a standard JSON library and created separate Newtonsoft and FSharp.Data JSON packages.
+* Dropped support for .Net Standard 2.0
+* Smaller breaking changes
+
+*)
