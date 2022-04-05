e22254937775eefb3ff6e7f24990c6d838712b32
SchlenkR <ronald.schlenker@binarygears.de>
2022-04-05 20:26:38 +0200

    package props Operators

--- src/FsHttp/Operators.fs
+++ src/FsHttp/Operators.fs
@@ -7,13 +7,5 @@ module Json =
     let (?) (json: JsonElement) (key: string) : JsonElement = json.GetProperty(key)
 
 module Operators =
-    open FsHttp.Helper
-
-    let (</>) = Url.combine
-
-    // TODO: Document this
-    type Kickoff = Kickoff with
-        static member inline ($) (Kickoff, x: FsHttp.Domain.IToRequest) =
-            x |> Request.send
-    let inline kickoff x = (($) Kickoff) x
-    let inline (~%) x = kickoff x
+    let ( </> ) = Helper.Url.combine
+    let ( ~% ) = Request.send
