c774edebd105c2baa47089b514cb97e84a9e3cfa
SchlenkR <ronald@pure-state.de>
2023-07-22 10:49:55 +0200

    fiddle

---
+++ fiddle/issue-126.fsx
@@ -0,0 +1,17 @@
+
+#r "../src/FsHttp/bin/debug/net7.0/fshttp.dll"
+
+open System.IO
+open System.Net.Http
+open FsHttp
+
+let request = task {
+    let! theContent = File.ReadAllTextAsync("c:/temp/content.txt")
+    return http {
+        GET "https://www.wikipedia.de"
+        config_transformHttpRequestMessage (fun msg ->
+            msg.Content <- new StringContent(theContent)
+            msg
+        )
+    }
+}
