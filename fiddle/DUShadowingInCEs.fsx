849b5e9ce88c971f21afa1dcfa6bd6be6592553d
SchlenkR <ronald@pure-state.de>
2023-07-22 11:06:57 +0200

    fiddle

---
+++ fiddle/DUShadowingInCEs.fsx
@@ -0,0 +1,28 @@
+#r "../src/FsHttp/bin/debug/net7.0/fshttp.dll"
+
+open System
+open FsHttp
+
+type ArgA =
+    | FileName of string
+    | Path of string
+
+type ArgB =
+    | FileName of string
+    | AnotherPath of string
+
+type IRequestContext<'self> with
+    [<CustomOperation("a")>]
+    member _.A(context: IRequestContext<MultipartContext>, name, [<ParamArray>] argA: ArgA array) : MultipartContext =
+        failwith "TODO: implement me"
+    
+    [<CustomOperation("b")>]
+    member _.B(context: IRequestContext<MultipartContext>, name, [<ParamArray>] argA: ArgB array) : MultipartContext =
+        failwith "TODO: implement me"
+
+http {
+    POST("")
+    multipart
+
+    a "Resources/uploadFile.txt" (FileName "xsycy") (Path "xsycy")
+}
